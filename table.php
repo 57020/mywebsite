ffd07c156a228ef979fe38763c943301b4644828
57020 <97946591+57020@users.noreply.github.com>
2022-02-18 10:42:37 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>วชิรวิชญ์ ชีวะอารี</td>
+        <td>ไบร์ท</td>
+        <td><img src="https://th.m.wikipedia.org/wiki/ไฟล์:Vachirawit_Chivaaree_at_GMMTV_2021_event.jpeg" width="130px"></td>
+        <td><a href="https://mobile.twitter.com/bbrightvc?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
+      </tr>  
+    </table>
+  </body> 
+</html>
