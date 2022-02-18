26927ea3d7c7be58c845c077d63a5d46fc33eab9
57020 <97946591+57020@users.noreply.github.com>
2022-02-18 11:23:06 +0700

    Update table.php

--- table.php
+++ table.php
@@ -16,37 +16,37 @@
         <td>วชิรวิชญ์ ชีวะอารี</td>
         <td>ไบร์ท</td>
         <td><img src="https://s.isanook.com/mv/0/rp/r/w728/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL212LzAvdWQvMTkvOTY2ODEvYmJyaWdodHZjXzg4MTgxNjU0XzQ5MjM3MzM4NDcuanBn.jpg" width="130px"></td>
-        <td><a href="https://mobile.twitter.com/bbrightvc?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
+        <td><a href="https://mobile.twitter.com/bbrightvc?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">@bbrightvc</a></td>
       <tr>
         <td>2</td>
         <td>เมธวิน โอภาสเอี่ยมขจร</td>
         <td>วิน</td>
         <td><img src="https://www.innnews.co.th/wp-content/uploads/2021/11/2704649668439863363-819x1024.jpg" width="130px"></td>
-        <td><a href="https://mobile.twitter.com/bbrightvc?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
+        <td><a href="https://mobile.twitter.com/winmetawin?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">@winmetawin</a></td>
       <tr>
         <td>3</td>
         <td>จิรวรรตน์ สุทธิวณิชศักดิ์</td>
         <td>ดิว</td>
         <td><img src="https://s.isanook.com/mv/0/ui/20/103161/a75ef9aecafe99ce4ea17df24513eb57_1600255427.jpg" width="130px"></td>
-        <td><a href="https://mobile.twitter.com/bbrightvc?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
+        <td><a href="https://mobile.twitter.com/dew_jsu">@dew_jsu</a></td>
       <tr>
         <td>4</td>
         <td>พิรพัฒน์ วัฒนเศรษสิริ</td>
         <td>ดิเอิร์ท</td>
         <td><img src="https://s.isanook.com/ca/0/rp/r/w728/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2NhLzAvdWQvMjgwLzE0MDM0NjgvdGhlZWFydGhlXzEzNzIxOTE3MjE1NjkyMDEyNjMuanBn.jpg" width="130px"></td>
-        <td><a href="https://mobile.twitter.com/bbrightvc?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
+        <td><a href="https://twitter.com/Earth_Pirapat?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">@Earth_Pirapat</a></td>
     <tr>
         <td>5</td>
         <td>สหภาพ วงศ์ราษฎร์</td>
         <td>มิกซ์ซิว</td>
         <td><img src="https://www.newtv.co.th/images/content/ct_20210419111518401.jpg" width="130px"></td>
-        <td><a href="https://mobile.twitter.com/bbrightvc?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
+        <td><a href="https://twitter.com/wixxiws">@wixxiws</a></td>
       <tr>
         <td>6</td>
         <td>ภวินท์ กุลการัณยวิชญ์</td>
         <td>ภวิน</td>
         <td><img src="https://i.pinimg.com/originals/79/31/1d/79311d4d8070998f0f7ec527d517dc75.jpg" width="130px"></td>
-        <td><a href="https://mobile.twitter.com/bbrightvc?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
+        <td><a href="https://twitter.com/Winpww">@Winpww</a></td>
       </tr>  
     </table>
   </body> 
