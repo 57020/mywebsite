cba02115e5e5c2e5b51cc3a3430e728b0cf48685
57020 <97946591+57020@users.noreply.github.com>
2022-02-18 11:44:14 +0700

    Create page2.php

---
+++ page2.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title>การตกแต่งเว็บไซต์ด้วย CSS</title>
+		<style>
+			#box1{
+				width: 800px;
+				height: 350px;
+			}
+			.box{
+				background-color: lightblue;
+			}
+                        body{
+                                background-color: pink;
+			}
+                        h1{
+                                color: yellow;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="box1" class="box">
+			<h1> นายวงศธร  สีดี เลขที่ 6 ม.4/13</h1>
+		</div>
+	</body>
+</html>
