7fb6718ea9176946f9983d390c104af79fea0c0d
57020 <97946591+57020@users.noreply.github.com>
2022-02-22 19:01:47 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>กำหนดรูปแบบตัวอักษร</title>
+  <style>
+    @font-face{
+      font-family: myFont1;
+      src: url()
+    }
+    @font-face{
+      font-family: myFont2;
+      src: url()
+    }
+    #font1{
+	font-family: myFont1;
+    }
+    #font2{
+	font-family: myFont2;
+    }
+  </style>
+</head>
+<body>
+  <h1 id="font1">นายวงศธร สีดี ชั้นมัธยมศึกษาปีที่ 4 ห้อง 13 เลขที่ 6 เลขประจำตัว 57020</h1>
+  <h1 id="font2">นายวงศธร สีดี ชั้นมัธยมศึกษาปีที่ 4 ห้อง 13 เลขที่ 6 เลขประจำตัว 57020</h1>
+</body>
+</html>
